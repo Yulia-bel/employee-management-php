ab516c41d2d7b9b983c77a6058976427f61255e5
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-07-28 11:42:33 +0200

    removed comented conditional

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -1,10 +1,12 @@
 <?php 
 
 require "employeeManager.php";
-if($_POST['action'] == "select"){
-    getEmployees();
-// } else if($_POST['action'] == "update"){
-//     updateEmployee();
-}
 
+if(isset($_POST['action'])){
+    if($_POST['action'] == "select"){
+        getEmployees();
+
+    }
+
+}
 ?>

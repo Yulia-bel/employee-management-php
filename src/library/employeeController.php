f8e45d1fa9d93ba87c57df2780d3d610379ce1b5
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-07-28 11:52:59 +0200

    saving variables received from js/creating array with all the properties from json and saving it it with values if in grid and empty if not in grid

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -6,7 +6,20 @@ if(isset($_POST['action'])){
     if($_POST['action'] == "select"){
         getEmployees();
 
-    }
+    }    if($_POST['action']=="addemployee") {
+        $newName = $_POST['name'];
+        $newEmail = $_POST['email'];
+        $newAge = $_POST['age'];
+        $newStreet = $_POST['street'];
+        $newCity = $_POST['city'];
+        $newState = $_POST['state'];
+        $newPostal = $_POST['postal'];
+        $newPhone = $_POST['phone'];
 
+        // save other variables recieved from js
+        $newUserArray = array("id"=>"", "name"=>$newName, "lastName"=>"", "email"=>$newEmail, "gender"=>"","age"=>$newAge, "city"=>$newCity, "sreetAddress"=>$newStreet, "state"=>$newState, "postalCode"=>$newPostal, "phoneNumber"=>$newPhone);
+
+        addEmployee($newUserArray);
+    }
 }
 ?>

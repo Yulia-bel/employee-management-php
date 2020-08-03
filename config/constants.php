1caf67ec349826c082e7665802669184f010a1cb
JaimeBotet <jaime.90.botet@gmail.com>
2020-08-03 16:10:19 +0200

    Rearrange files and folders to MVC architecture

---
+++ config/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+$documentRoot = getcwd();
+
+//BASE PATH -> FOR REFERENCE FILES
+define("BASE_PATH", $documentRoot);
+
+//CONTROLLERS
+define("CONTROLLERS", BASE_PATH . '/controllers/');
+
+//VIEWS
+define("VIEWS", BASE_PATH . '/views/');
+
+//MODELS
+define("MODELS", BASE_PATH . '/models/');
+
+//RESOURCES
+define("RESOURCES", BASE_PATH . '/resources/');
---
+++ views/login/login.php
@@ -0,0 +1,68 @@
+<!-- TODO Application entry point. Login view -->
+
+<html lang="en">
+
+<?php
+
+session_start();
+if (isset($_GET['logout'])) {
+  echo "<script type='text/javascript'>alert('Your session expired!');</script>";
+}
+
+if (isset($_SESSION['userId'])) {
+  header('Location: src/dashboard.php');
+}
+?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Employee Managment</title>
+  <!-- OWN STYLE -->
+  <link href="css/login.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+  <!-- DEPENDENCIES - JQUERY AND BOOTSTRAP-->
+  <script src="node_modules/jquery/dist/jquery.js"></script>
+  <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+  <link href="node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+  <script src="assets/login.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center mb-5">Sign In</h5>
+
+            <form class="form-signin">
+
+              <div class="form-label-group">
+                <input type="text" id="inputName" name="email" class="form-control mb-3" placeholder="Username" required autofocus>
+              </div>
+
+              <div class="form-label-group">
+                <input type="password" id="inputPassword" name="password" class="form-control mb-3" placeholder="Password" required>
+              </div>
+
+              <div class="form-label-group invisible" id="error_message">
+
+              </div>
+
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" id="login">Sign in</button>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script type="text/javascript" src="node_modules/jsgrid/dist/jsgrid.min.js"></script>
+
+</body>
+
+
+</html>

ef6f975d6a1c6e3ed57fe9a0af9acf17bfb8dbc6
Yulia Belyakova <yuliya.beliakova@gmail.com>
2020-09-03 14:06:45 +0200

    unused files deleted

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-
-//require_once 'libs/classes/database.php';
 require_once 'libs/classes/controller.php';
 require_once 'libs/classes/view.php';
 require_once 'libs/classes/model.php';
 require_once 'libs/classes/router.php';
-require_once 'libs/database.php';
+require_once 'libs/classes/database.php';
 
 require_once 'config/config.php';
 

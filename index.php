cd42b7cc0d79189dfc069f0cd9b0f3e600c17907
Yulia Belyakova <yuliya.beliakova@gmail.com>
2020-09-03 13:55:12 +0200

    router renamed

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 require_once 'libs/classes/controller.php';
 require_once 'libs/classes/view.php';
 require_once 'libs/classes/model.php';
-require_once 'libs/classes/app.php';
+require_once 'libs/classes/router.php';
 require_once 'libs/database.php';
 
 require_once 'config/config.php';
